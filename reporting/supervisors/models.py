60c1c09196038b40886324d1013be74ded1d8dbd
Peter Reutemann <fracpete@gmail.com>
2017-02-14 17:08:03 +1300

    simple model for start/end dates of students

--- reporting/supervisors/models.py
+++ reporting/supervisors/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class StudentDates(models.Model):
+    """
+    Start/end dates for students.
+    """
+    student_id = models.CharField(max_length=20)
+    program = models.CharField(max_length=4)
+    start_date = models.DateField()
+    end_date = models.DateField()
