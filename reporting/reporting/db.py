7a9032bb74e024c5da3e0ed63e84787161c65e23
Peter Reutemann <fracpete@gmail.com>
2017-02-23 14:13:46 +1300

    added methods for retrieving cells from dictionary using multiple aliases and default value, e.g., from CSV

--- reporting/reporting/db.py
+++ reporting/reporting/db.py
@@ -20,3 +20,62 @@ def truncate_strings(row, max):
     for k in row.keys():
         if len(row[k]) > max:
             row[k] = row[k][:max]
+
+def string_cell(row, names, defvalue=None):
+    """
+    Returns a string cell from the row dictionary, using a list of aliases for the value.
+
+    :param row: the (eg CSV) row dictionary
+    :type row: dict
+    :param names: the list of alias names
+    :type names: list of str
+    :param defvalue: the default value
+    :type defvalue: str
+    :return: the value if any of the aliases was found, otherwise the default value
+    :rtype: str
+    """
+    result = defvalue
+    for name in names:
+        if name in row:
+            return row[name]
+    return result
+
+def int_cell(row, names, defvalue=None):
+    """
+    Returns an int value from the row dictionary, using a list of aliases for the value.
+
+    :param row: the (eg CSV) row dictionary
+    :type row: dict
+    :param names: the list of alias names
+    :type names: list of str
+    :param defvalue: the default value
+    :type defvalue: int
+    :return: the value if any of the aliases was found, otherwise the default value
+    :rtype: int
+    """
+    result = defvalue
+    for name in names:
+        if name in row:
+            if row[name] != '':
+                return int(row[name])
+    return result
+
+def float_cell(row, names, defvalue=None):
+    """
+    Returns a float value from the row dictionary, using a list of aliases for the value.
+
+    :param row: the (eg CSV) row dictionary
+    :type row: dict
+    :param names: the list of alias names
+    :type names: list of str
+    :param defvalue: the default value
+    :type defvalue: float
+    :return: the value if any of the aliases was found, otherwise the default value
+    :rtype: float
+    """
+    result = defvalue
+    for name in names:
+        if name in row:
+            if row[name] != '':
+                return float(row[name])
+    return result
