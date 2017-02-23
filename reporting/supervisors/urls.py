d7c1f300147d2dfb040ab4744f23a9c47a948621
Peter Reutemann <fracpete@gmail.com>
2017-02-23 22:10:13 +1300

    added some simple output for supervisors register

--- reporting/supervisors/urls.py
+++ reporting/supervisors/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^list$', views.list, name='list'),
+    url(r'^list$', views.list_supervisors, name='list'),
 ]
--- reporting/supervisors/views.py
+++ reporting/supervisors/views.py
@@ -27,7 +27,7 @@ def index(request):
     context['years_back'] = YEARS_BACK
     return HttpResponse(template.render(context, request))
 
-def list(request):
+def list_supervisors(request):
     # get parameters
     if "school" not in request.POST:
         return create_error_response(request, 'supervisors', 'No school defined!')
@@ -37,8 +37,29 @@ def list(request):
         return create_error_response(request, 'supervisors', 'No number of years back defined!')
     years_back = int(request.POST["years_back"])
 
-    # TODO
+    sql = """
+        select sd.school, s.supervisor, s.student_id
+        from supervisors_studentdates sd, supervisors_supervisors s
+        where sd.student_id = s.student_id
+        and sd.school in ('%s')
+        group by sd.school, s.supervisor, s.student_id
+        order by sd.school, s.supervisor, s.student_id
+        """ % ("','".join(schools))
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = {}
+    school_set = set()
+    for row in cursor.fetchall():
+        school = row[0]
+        school_set.add(school)
+        if school not in result:
+            result[school] = []
+        result[school].append({'supervisor': row[1], 'student': row[2]})
+    school_list = list(school_set)
+    sorted(school_list)
 
     template = loader.get_template('supervisors/list.html')
     context = applist.template_context('supervisors')
+    context['schools'] = school_list
+    context['results'] = result
     return HttpResponse(template.render(context, request))
