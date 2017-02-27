8b152dd4448d0472475f9d188ef9056ba0ef79f9
Peter Reutemann <fracpete@gmail.com>
2017-02-28 11:58:49 +1300

    taking years into account now for listing supervisors

--- reporting/supervisors/views.py
+++ reporting/supervisors/views.py
@@ -6,6 +6,7 @@ import reporting.applist as applist
 from reporting.error import create_error_response
 import traceback
 import sys
+from datetime import date
 
 YEARS_BACK = 5
 
@@ -20,11 +21,24 @@ def index(request):
     schools = []
     for row in cursor.fetchall():
         schools.append(row[0])
+    # get year from earliest start date
+    cursor.execute("""
+        select extract(year from min(start_date))
+        from supervisors_studentdates
+    """)
+    min_year = None
+    max_years = None
+    for row in cursor.fetchall():
+        min_year = row[0]
+    if min_year is not None:
+        max_years = date.today().year - min_year
     # configure template
     template = loader.get_template('supervisors/index.html')
     context = applist.template_context('supervisors')
     context['schools'] = schools
     context['years_back'] = YEARS_BACK
+    if max_years is not None:
+        context['max_years'] = int(max_years)
     return HttpResponse(template.render(context, request))
 
 def list_supervisors(request):
@@ -36,15 +50,17 @@ def list_supervisors(request):
     if "years_back" not in request.POST:
         return create_error_response(request, 'supervisors', 'No number of years back defined!')
     years_back = int(request.POST["years_back"])
+    start_year = date.today().year - years_back
 
     sql = """
         select sd.school, s.supervisor, s.student_id
         from supervisors_studentdates sd, supervisors_supervisors s
         where sd.student_id = s.student_id
         and sd.school in ('%s')
+        and sd.start_date >= '%s-01-01'
         group by sd.school, s.supervisor, s.student_id
         order by sd.school, s.supervisor, s.student_id
-        """ % ("','".join(schools))
+        """ % ("','".join(schools), str(start_year))
     cursor = connection.cursor()
     cursor.execute(sql)
     result = {}
