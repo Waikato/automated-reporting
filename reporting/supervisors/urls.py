1e41d74a2a76e5a3153dd09f851bed369568a5bf
Peter Reutemann <fracpete@gmail.com>
2017-04-03 10:57:53 +1200

    split supervisor search into: faculity, supervisor, student

--- reporting/supervisors/urls.py
+++ reporting/supervisors/urls.py
@@ -4,5 +4,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^list$', views.list_supervisors, name='list'),
+
+    url(r'^search-by-faculty', views.search_by_faculty, name='search-by-faculty'),
+    url(r'^list-by-faculty$', views.list_by_faculty, name='list-by-faculty'),
+
+    url(r'^search-by-supervisor$', views.search_by_supervisor, name='search-by-supervisor'),
+    url(r'^list-by-supervisor$', views.list_by_supervisor, name='list-by-supervisor'),
+
+    url(r'^search-by-student$', views.search_by_student, name='search-by-student'),
+    url(r'^list-by-student$', views.list_by_student, name='list-by-student'),
 ]
--- reporting/supervisors/views.py
+++ reporting/supervisors/views.py
@@ -6,6 +6,7 @@ from reporting.models import GradeResults
 
 import reporting.applist as applist
 from reporting.error import create_error_response
+from reporting.form_utils import check_variable_presence, get_variable
 import traceback
 import sys
 from datetime import date
@@ -18,10 +19,10 @@ def index(request):
     # get all schools
     cursor = connection.cursor()
     cursor.execute("""
-        SELECT DISTINCT(owning_school_clevel)
+        SELECT DISTINCT(school)
         FROM %s
-        ORDER BY owning_school_clevel ASC
-        """ % GradeResults._meta.db_table)
+        ORDER BY school ASC
+        """ % StudentDates._meta.db_table)
     schools = []
     for row in cursor.fetchall():
         schools.append(row[0])
@@ -44,7 +45,39 @@ def index(request):
     context['max_years'] = int(max_years) if max_years is not None else YEARS_BACK
     return HttpResponse(template.render(context, request))
 
-def add_student(data, school, department, supervisor, studentid, program):
+def search_by_faculty(request):
+    # get parameters
+    response = check_variable_presence(request, 'supervisors', 'school')
+    if response is not None:
+        return response
+    schools = get_variable(request, 'school', as_list=True)
+
+    response = check_variable_presence(request, 'supervisors', 'years_back')
+    if response is not None:
+        return response
+    years_back = int(get_variable(request, 'years_back'))
+
+    sql = """
+        select distinct(owning_department_clevel)
+        from %s
+        where owning_school_clevel in ('%s')
+        order by owning_department_clevel
+        """ % (GradeResults._meta.db_table, "','".join(schools))
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    departments = []
+    for row in cursor.fetchall():
+        departments.append(row[0])
+
+    # configure template
+    template = loader.get_template('supervisors/search_by_faculty.html')
+    context = applist.template_context('supervisors')
+    context['schools'] = schools
+    context['departments'] = departments
+    context['max_years'] = years_back
+    return HttpResponse(template.render(context, request))
+
+def add_student(data, school, department, supervisor, studentid, program, supervisor_type, study_type, only_current):
     """
     Adds the student to the "data" structure. Overview of the data structure (<name> is
     a key in a dictionary):
@@ -72,9 +105,22 @@ def add_student(data, school, department, supervisor, studentid, program):
     :type supervisor: str
     :param studentid: the student id
     :type studentid: str
-    :param program: the program (PD/MD)
+    :param program: the program (PhD/Master)
     :type program: str
+    :param supervisor_type: the type of supervisors to list ["chief", "other"]
+    :type supervisor_type: list
+    :param study_type: list of study type ["full", "part"]
+    :type study_type: list
+    :param only_current: whether to list only current students
+    :type only_current: bool
     """
+
+    if program == "DP":
+        program_display = "PhD"
+    elif program == "MD":
+        program_display = "Master"
+    else:
+        program_display = program
 
     # ensure data structures are present
     if school not in data:
@@ -83,14 +129,11 @@ def add_student(data, school, department, supervisor, studentid, program):
         data[school][department] = {}
     if supervisor not in data[school][department]:
         data[school][department][supervisor] = {}
-    if program not in data[school][department][supervisor]:
-        data[school][department][supervisor][program] = {}
-    if studentid not in data[school][department][supervisor][program]:
-        data[school][department][supervisor][program][studentid] = {}
+    if program_display not in data[school][department][supervisor]:
+        data[school][department][supervisor][program_display] = {}
 
     # load student data
     today = date.today().strftime("%Y-%m-%d")
-    sdata = data[school][department][supervisor][program][studentid]
     for s in StudentDates.objects.all().filter(school=school, department=department, student_id=studentid, program=program):
         sname = None
         for g in GradeResults.objects.all().filter(student_id=studentid):
@@ -105,57 +148,91 @@ def add_student(data, school, department, supervisor, studentid, program):
             full_time = 'Yes'
         else:
             full_time = 'No'
+
+        end_date = s.end_date.strftime("%Y-%m-%d")
+        if end_date == "9999-12-31" or end_date <= "1900-01-01":
+            end_date = "N/A"
+
+        print(studentid, s.status)
+        status = s.status
+        if status is None:
+            if end_date == "N/A":
+                status = "current"
+            elif s.end_date.strftime("%Y-%m-%d") >= today:
+                status = "current"
+            else:
+                status = "finished"
+
+        # check conditions
+        if chief == "Yes" and "chief" not in supervisor_type:
+            continue
+        if chief == "No" and "other" not in supervisor_type:
+            continue
+        if full_time == "Yes" and "full" not in study_type:
+            continue
+        if full_time == "No" and "part" not in study_type:
+            continue
+        if only_current and status == "finished":
+            continue
+
+        if studentid not in data[school][department][supervisor][program_display]:
+            data[school][department][supervisor][program_display][studentid] = {}
+
+        sdata = data[school][department][supervisor][program_display][studentid]
         sdata['name'] = sname
         sdata['start_date'] = s.start_date.strftime("%Y-%m-%d")
-        sdata['end_date'] = s.end_date.strftime("%Y-%m-%d")
-        if sdata['end_date'] == "9999-12-31" or sdata['end_date'] <= "1900-01-01":
-            sdata['end_date'] = "N/A"
+        sdata['end_date'] = end_date
         sdata['months'] = s.months
         sdata['full_time'] = full_time
         sdata['chief_supervisor'] = chief
-        if s.status is None:
-            if sdata['end_date'] == "N/A":
-                sdata['status'] = "current"
-            elif s.end_date.strftime("%Y-%m-%d") >= today:
-                sdata['status'] = "current"
-            else:
-                sdata['status'] = "finished"
-        else:
-            sdata['status'] = s.status
+        sdata['status'] = status
 
-def list_supervisors(request):
+def list_by_faculty(request):
     # get parameters
-    if "school" not in request.POST:
-        return create_error_response(request, 'supervisors', 'No school defined!')
-    schools = request.POST.getlist("school")
+    response = check_variable_presence(request, 'supervisors', 'school')
+    if response is not None:
+        return response
+    schools = get_variable(request, 'school', as_list=True)
 
-    if "years_back" not in request.POST:
-        return create_error_response(request, 'supervisors', 'No number of years back defined!')
-    years_back = int(request.POST["years_back"])
+    response = check_variable_presence(request, 'supervisors', 'department')
+    if response is not None:
+        return response
+    departments = get_variable(request, 'department', as_list=True)
+
+    response = check_variable_presence(request, 'supervisors', 'years_back')
+    if response is not None:
+        return response
+    years_back = int(get_variable(request, 'years_back'))
     start_year = date.today().year - years_back
 
-    if "csv" not in request.POST:
-        export = None
-    else:
-        export = "csv"
+    programs = get_variable(request, 'programs', as_list=True, def_value=["DP", "MD"])
+    supervisor_type = get_variable(request, 'supervisor_type', as_list=True, def_value=["chief", "other"])
+    study_type = get_variable(request, 'study_type', as_list=True, def_value=["full", "part"])
+    only_current = get_variable(request, 'only_current', def_value="off") == "on"
+    export = get_variable(request, 'csv')
 
     sql = """
         select sd.school, sd.department, s.supervisor, s.student_id, sd.program
         from %s sd, %s s
         where sd.student_id = s.student_id
         and sd.school in ('%s')
+        and sd.department in ('%s')
         and sd.start_date >= '%s-01-01'
         and s.active = True
         group by sd.school, sd.department, s.supervisor, s.student_id, sd.program
         order by sd.school, sd.department, s.supervisor, s.student_id, sd.program
-        """ % (StudentDates._meta.db_table, Supervisors._meta.db_table, "','".join(schools), str(start_year))
+        """ % (StudentDates._meta.db_table, Supervisors._meta.db_table, "','".join(schools), "','".join(departments), str(start_year))
     cursor = connection.cursor()
-    print(sql)
     cursor.execute(sql)
     result = {}
     for row in cursor.fetchall():
         try:
-            add_student(data=result, school=row[0], department=row[1], supervisor=row[2], studentid=row[3], program=row[4])
+            print(row[4], programs)
+            if row[4] not in programs:
+                continue
+            add_student(data=result, school=row[0], department=row[1], supervisor=row[2], studentid=row[3],
+                        program=row[4], supervisor_type=supervisor_type, study_type=study_type,
+                        only_current=only_current)
         except Exception as ex:
             print("row=" + str(row))
             traceback.print_exc(file=sys.stdout)
@@ -163,7 +240,7 @@ def list_supervisors(request):
     # CSV or HTML?
     if export == "csv":
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="supervisors-%s.csv"' % date.today().strftime("%Y-%m-%d")
+        response['Content-Disposition'] = 'attachment; filename="faculty-%s.csv"' % date.today().strftime("%Y-%m-%d")
         writer = csv.writer(response)
         writer.writerow([
             'Faculty/School',
@@ -201,7 +278,73 @@ def list_supervisors(request):
                             ])
         return response
     else:
-        template = loader.get_template('supervisors/list.html')
+        template = loader.get_template('supervisors/list_by_faculty.html')
         context = applist.template_context('supervisors')
         context['results'] = result
+        return HttpResponse(template.render(context, request))
+
+def search_by_supervisor(request):
+    # get parameters
+
+    # TODO
+
+    # configure template
+    template = loader.get_template('supervisors/search_by_supervisor.html')
+    context = applist.template_context('supervisors')
+    return HttpResponse(template.render(context, request))
+
+def list_by_supervisor(request):
+    # get parameters
+    # TODO
+
+    export = get_variable(request, 'csv')
+
+    # CSV or HTML?
+    if export == "csv":
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="supervisor-%s.csv"' % date.today().strftime("%Y-%m-%d")
+        writer = csv.writer(response)
+        writer.writerow([
+            'Blah',
+            # TODO headers
+        ])
+        # TODO data
+        return response
+    else:
+        template = loader.get_template('supervisors/list_by_supervisor.html')
+        context = applist.template_context('supervisors')
+        context['results'] = None  # TODO
+        return HttpResponse(template.render(context, request))
+
+def search_by_student(request):
+    # get parameters
+
+    # TODO
+
+    # configure template
+    template = loader.get_template('supervisors/search_by_student.html')
+    context = applist.template_context('supervisors')
+    return HttpResponse(template.render(context, request))
+
+def list_by_student(request):
+    # get parameters
+    # TODO
+
+    export = get_variable(request, 'csv')
+
+    # CSV or HTML?
+    if export == "csv":
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="student-%s.csv"' % date.today().strftime("%Y-%m-%d")
+        writer = csv.writer(response)
+        writer.writerow([
+            'Blah',
+            # TODO headers
+        ])
+        # TODO data
+        return response
+    else:
+        template = loader.get_template('supervisors/list_by_student.html')
+        context = applist.template_context('supervisors')
+        context['results'] = None  # TODO
         return HttpResponse(template.render(context, request))
