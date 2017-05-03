01f954e5981fb51fd3a09885c237e7cd6aeb32e5
Peter Reutemann <fracpete@gmail.com>
2017-05-03 16:02:23 +1200

    fixed typo

--- reporting/reporting/settings.py
+++ reporting/reporting/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5)!dc%xc6622p!!wa54qaf+$_8v5a29ax04+$b)nc8x@-jtu$_'
 
-# custom database settings?
+# custom debug settings?
 try:
     import reporting.settings_debug
     DEBUG = reporting.settings_debug.DEBUG
