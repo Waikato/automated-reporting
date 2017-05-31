113fd49eb082e54a2a52436f6835942acf6ec1c4
Peter Reutemann <fracpete@gmail.com>
2017-06-01 10:59:34 +1200

    Clint's changes

--- reporting/reporting/settings.py
+++ reporting/reporting/settings.py
@@ -158,7 +158,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = '/var/www/html/reporting/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     'reporting/static/',
