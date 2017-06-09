53dcc917ef76b00210bd8894270716825486cc1b
Peter Reutemann <fracpete@gmail.com>
2017-06-09 12:19:59 +1200

    added model for templates

--- reporting/hr_forms/models.py
+++ reporting/hr_forms/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class FormTemplate(models.Model):
+    """
+    Class for defining a PDF form with its overlay.
+    """
+
+    name = models.CharField(max_length=250, db_index=True)
+    description = models.TextField()
+    version = models.CharField(max_length=50, db_index=True)
+    pdf = models.BinaryField()
+    overlay = models.TextField()
+
+    class Meta:
+        permissions = (
+            ("can_access_formtemplate", "Can access Form Template"),
+            ("can_manage_formtemplate", "Can manage Form Template"),
+        )
